ff420e22a3a51752cb5b3d7dd6cfdd0bbaaa83df
Jac Thomas <jacthomas@Jacs-MacBook-Pro-2.local>
2019-10-28 14:27:58 +0100

    tidy app.js

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-// requirejs.config({
-//     baseUrl: 'node_modules',
-//     paths: {
-//         app: '../app'
-//     }
-// });
-//
-// // Start loading the main app file. Put all of
-// // your application logic in there.
-// requirejs(['app/server']);
-
-
 const express = require('express')
 const app = express()
 const port = 3000
@@ -27,22 +15,7 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-// app.get("/", function (req, res) {
-//     res.sendFile(__dirname + '/views/index.html');
-// });
 
-
-
-
-// app.get(/[\s\S]*/, function (req, res) {
-//     var url = require('url');
-//     var url_parts = url.parse(req.url, true);
-//     var query = url_parts.query;
-//     var date = new Date(query.toString);
-//     res.json({"unix": date.getTime(), "utc" : date.toUTCString() });
-//     res.send(req.query)
-// });
 
 app.get('/:date_string?', function (req, res) {
 
@@ -61,16 +34,7 @@ app.get('/:date_string?', function (req, res) {
     res.send(res.json({"unix": date.getTime(), "utc" : date.toUTCString() }))
 });
 
-/[\s\S]*/
-
-// your first API endpoint...
 app.get("/api/hello", function (req, res) {
     res.json({greeting: 'hello API'});
 });
 
-//http://localhost:3000/
-//
-// // listen for requests :)
-// var listener = app.listen(process.env.PORT, function () {
-//     console.log('Your app is listening on port ' + listener.address().port);
-// });
